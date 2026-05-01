d2cb36165f41af351a1f8e616362fdde35ab3465
mariami57 <mmitova3@gmail.com>
2026-05-01 12:52:15 +0300

    added counter component

--- hotelinventoryapp/src/app/home/home.ts
+++ hotelinventoryapp/src/app/home/home.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { Greeting } from '../components/greeting/greeting';
+import { Counter } from '../components/counter/counter';
 
 @Component({
   selector: 'app-home',
-  imports: [Greeting],
+  imports: [Greeting, Counter],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
   homeMessage = signal('Hello world!');
+  keyUpHandler(event: KeyboardEvent) {
+    console.log(`user pressed the ${event.key} key`);
+  }
 }
