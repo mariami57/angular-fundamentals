0e717fb0f9a5ce5d6112fcce27ed2852ab7f2235
mariami57 <mmitova3@gmail.com>
2026-05-01 19:40:45 +0300

    added services

---
+++ hotelinventoryapp/src/app/services/todos.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Todo } from '../model/todo.type';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodosService {
+  todoItems: Array<Todo> = [{
+    title: 'Todo 1',
+    completed: false,
+    userId: 1,
+    id: 1
+  }, {
+    title: 'Todo 2',
+    completed: true,
+    userId: 1,
+    id: 2
+  }];
+
+  constructor() { }
+}
